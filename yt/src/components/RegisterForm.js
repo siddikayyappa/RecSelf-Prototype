1b4d6c04bdfea927f83cd8c62abd1859999048db
siddik11803-IIITH <siddik.naraharisetti@students.iiit.ac.in>
2024-04-24 21:10:01 +0530

    registeration done

--- yt/src/components/RegisterForm.js
+++ yt/src/components/RegisterForm.js
@@ -1,21 +1,42 @@
 import React, { useState } from 'react';
 import {Button, Form} from "react-bootstrap";
-// Import AuthService if you have one
+import axios from 'axios';
+
+
 
 function RegisterForm() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [error, setError] = useState(null);
     
     const handleSubmit = (e) => {
         e.preventDefault();
         // Register Service
+        // Send https request post
+        const data = {
+            "name": name,
+            "email": email,
+            "password": password
+        }
+        console.log(data);
+        axios.post('http://localhost:4000/register', data)
+        .then((response) => {
+            console.log(response);
+            setError("User Registered Successfully Please Login Now!");
+        })
+        .catch((error) => {
+            console.log(error);
+            setError("An Error has occured: ", error.message);
+        });
+
     }
 
     return (
         <Form>
         <Form.Group controlId="formBasicName">
             <Form.Label>Name</Form.Label>
-            <Form.Control className="w-50 center" type="text" placeholder="Enter Name" />
+            <Form.Control className="w-50 center" type="text" placeholder="Enter Name" value={name} onChange={(e) => setName(e.target.value)}/>
             <Form.Text className="text-muted">
             </Form.Text>
         </Form.Group>
@@ -36,6 +57,7 @@ function RegisterForm() {
         <Button variant="primary" type="submit" onClick={handleSubmit}>
             Register
         </Button>
+        {error && <p>{error}</p>}
         </Form>
     );
 }
